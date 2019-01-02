746c0f4f81a576ba19a4193e4dbff341fd1f39b6
Pb <scandium@me.com>
2019-01-02 18:39:53 +0200

    move iconv def to defs.h

--- src/defs.h
+++ src/defs.h
@@ -8,6 +8,13 @@
 #define VMA2OFFS(vma) (assert((vma) >= ROM_BASE && (vma) - ROM_BASE <= 0x1ffffff), (vma) - ROM_BASE)
 #define OFFS2VMA(offs) (assert((offs) <= 0x1ffffff), (offs) + ROM_BASE)
 
+#ifndef HAS_ICONV
+typedef int iconv_t;
+#else
+#include <iconv.h>
+#endif
+
+
 /* TODO */
 #if 0
 struct rom_desc {
--- src/strtab.h
+++ src/strtab.h
@@ -1,16 +1,12 @@
 #ifndef STRTAB_H
 #define STRTAB_H
 
+#include "defs.h"
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
-
-#ifndef HAS_ICONV
-typedef int iconv_t;
-#else
-#include <iconv.h>
-#endif
 
 bool strtab_dump(const uint8_t* rom, uint32_t vma, uint32_t idx, bool has_idx, FILE* fout);
 bool strtab_dec_str(const uint8_t* strtab, uint32_t idx, char* out, size_t out_sz, size_t* nwritten,
